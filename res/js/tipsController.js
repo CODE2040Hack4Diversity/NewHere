e5ded547f119097b272315ebec6638a54cc2e966
CharlesJeremy <mulemi.charles29@gmail.com>
2015-07-11 21:53:37 -0700

    All upvote and downvote logic now making sense

--- res/js/tipsController.js
+++ res/js/tipsController.js
@@ -1,10 +1,22 @@
-var allComments = []
-var singleComment = {
-	author:"Christian Wallker",
-	commentID: 0,
-	upvotes:1000,
-	downvotes:0
-};
+var allTips = [];
+
+function tip(){
+	this.comment = "";
+	this.author = "";
+	this.upvotes = 1000;
+	this.downvotes = 0;
+}
+
+function staticTips(){
+	for(x = 0; x < 10; x ++) {
+		var newTip = new tip();
+		newTip.comment = "Code 2040 is the place to be";
+		newTip.author = "Tristan Walker";
+		newTip.upvotes = x;
+		newTip.downvotes = 10 - x;
+		allTips[x] = newTip;
+	}
+}
 
 
 
@@ -12,38 +24,90 @@ $("#get-sf-more-info").click(function(){
     $("#sf-more-info").slideToggle();
   })
 
-  window.onload = function(){
-    for(x=0; x<9;x++) {
-       var tip = document.createElement('div');
-       tip.className = "mdl-cell mdl-cell--7-col";
-
-       var innerHtml =    "<div class=\"mdl-cell mdl-cell--12-col-desktop\">" +
-                          "<div class=\"mdl-card mdl-shadow--2dp tip-card\">" +
-                          "<div class=\"mdl-card__supporting-text\">"+
-                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
-                          "Mauris sagittis pellentesque lacus eleifend lacinia..." +
-                          "<div class=\"interactive\">" +
-                          "<div class=\"arrow-up\" id=\"upvote" + x.toString()  + "\"></div>" +
-                          "<div class=\"arrow-down\" id=\"downvote" + x.toString()  + "\"></div>" +
-                          "</div>" +
-                          "</div>" +
-                          "</div>" +
-                          "</div>";
-      tip.innerHTML = innerHtml; 
-      document.getElementById('tipsContainer').appendChild(tip);
-
-      var upvote = document.getElementById("upvote" + x.toString());
-      var downvote = document.getElementById("downvote" + x.toString());
-
-      upvote.onclick = function() {
-  		alert("Clicked");
-	  }
-
-	  downvote.onclick = function() {
-  		alert("Clicked");
-	  }
-     }
+ window.onload = function(){
+ 	  staticTips();
+      renderTips();      
  }
+
+ function renderTips(){
+ 	for(x=0; x < allTips.length; x++) {
+   		var tip = document.createElement('div');
+   		tip.className = "mdl-cell mdl-cell--7-col";
+
+   		var innerHtml = "<div class=\"mdl-cell mdl-cell--12-col-desktop\">" +
+                           "<div class=\"mdl-card mdl-shadow--2dp tip-card\">" +
+                                "<div class=\"mdl-card__title\">" +
+    				                "<h2 class=\"mdl-card__title-text\">" + allTips[x].author + "</h2>" +
+  					            "</div>" +
+                                "<div class=\"mdl-card__supporting-text\">" +
+                      				  allTips[x].comment + 
+                      			"</div>" +
+                                "<div class=\"interactive\">" +
+                                     "<div class=\"arrow-up\" id=\"upvote" + x.toString()  + "\">" + allTips[x].upvotes + "</div>" +
+                                     "<div class=\"arrow-down\" id=\"downvote" + x.toString()  + "\">" + allTips[x].downvotes + "</div>" +
+                                "</div>" +
+                            "</div>" +
+                     "</div>";
+
+  		tip.innerHTML = innerHtml; 
+  		document.getElementById('tipsContainer').appendChild(tip);
+
+  		var upvote = document.getElementById("upvote" + x.toString());
+  		var downvote = document.getElementById("downvote" + x.toString());
+
+  		upvote.onclick = function() {
+  			increaseVotes();
+ 		 }
+
+  		downvote.onclick = function() {
+			decreaseVotes();
+  		}
+  	}
+ }
+
+ function compare(a ,b ){
+ 	var aPopularity = a.upvotes - a.downvotes;
+ 	var bPopularity = b.upvotes - b.downvotes;
+ 	if(aPopularity > bPopularity){
+ 		return 1;
+ 	}else if(aPopularity < bPopularity){
+ 		return -1;
+ 	}else{
+ 		return 0;
+ 	}
+ }
+
+ function increaseVotes(){
+ 	var amt = 0;
+ 	var thisTip = allTips[amt];
+ 	thisTip.upvotes = thisTip.upvotes + 1;
+ 	allTips[amt] = thisTip;
+ 	allTips.sort(compare);
+ 	document.getElementById("tipsContainer").innerHTML = "";
+ 	renderTips();
+ }
+
+
+
+ function decreaseVotes(){
+ 	var amt = 0;
+    var thisTip = allTips[amt];
+    thisTip.downvotes = thisTip.downvotes + 1;
+ 	allTips[amt] = thisTip;
+ 	allTips.sort(compare)
+ 	document.getElementById("tipsContainer").innerHTML = "";
+ 	renderTips();
+ }
+
+ 
+
+ 
+
+
+
+
+
+
 
 
 
