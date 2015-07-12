72cede812a117dd2493324c8e9f9e89ac619bb13
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-07-11 21:53:20 -0700

    made categories more standardize

--- res/js/tipsController.js
+++ res/js/tipsController.js
@@ -7,10 +7,31 @@ var singleComment = {
 };
 
 
+$(function() {
+var body = $('.layout-transparent');
+var backgrounds = new Array(
+'url(http://static.panoramio.com/photos/large/25275250.jpg)',
+'url(http://static1.squarespace.com/static/533c71fbe4b063017f750d46/t/533c72e5e4b0d9e2fee6b8ee/1392937462786/New+York+Purple+Skyline+1500px.jpg)'
+);
+var current = 0;
+
+function nextBackground() {
+body.css(
+'background',
+backgrounds[current = ++current % backgrounds.length]
+);
+
+setTimeout(nextBackground, 5000);
+}
+setTimeout(nextBackground, 5000);
+body.css('background', backgrounds[0]);
+});
+
+
 
 $("#get-sf-more-info").click(function(){
     $("#sf-more-info").slideToggle();
-  })
+
 
   window.onload = function(){
     for(x=0; x<9;x++) {
